cb20a377e50ad663dd728daa7d5331943bc8630b
Akash Das <mfs.akash@gmail.com>
2022-12-19 23:25:47 +0530

    added the curated healthcare database

---
+++ resources/db-dumps/healthcare-curated/HealthcareTables/InsuranceCompany/InsuranceCompany.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS InsuranceCompany;
+CREATE TABLE InsuranceCompany(
+   companyID   INTEGER  PRIMARY KEY 
+  ,companyName VARCHAR(100)
+  ,addressID   INTEGER 
+);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (6942,'Niva Bupa Health Insurance co ltd',404783);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (1933,'HDFC ERGO General Insurance Company Ltd.',126025);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (4872,'Raheja QBE General Insurance Co. Ltd.',172853);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (7130,'Max Bupa Health Insurance Co. Ltd.',524669);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (6951,'Star Health & Allied Insurance Co. Ltd.�',828030);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (5173,'Acko General Insurance Ltd.�',324341);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (7923,'Aditya Birla Health Insurance Co. Ltd',430879);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (8983,'SBI General Insurance Co.Ltd.',487027);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (4987,'Kotak Mahindra General Insurance Co. Ltd.',492266);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (6293,'Care Health Insurance Co. Ltd',823808);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (6772,'Future Generali Indis Insurance Co. Ltd',848832);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (8267,'The Oriental��Insurance Co. Limited',750508);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (1118,'Star Health and Allied Insurrance Co. Ltd.',919296);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (7580,'Care Health Insurance Co. Ltd�',274639);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (4559,'Tata AIG General Insurance Co. Ltd.',617847);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (2725,'Tata AIG General Insuarnce Co Ltd',674196);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (8799,'Universal Sompo GIC',548081);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (9201,'Go Digit General Insurance Co.Ltd',770578);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (9822,'United India Insurance Co.Ltd.',431422);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (9954,'Navi General Insurance Ltd.',463864);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (8136,'Edelweiss General Insurance Co. Ltd.',288674);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (8737,'Aditya Birla Health Insurance Co. Ltd.',450169);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (6642,'Cholamandalam MS General Insurance Co. Ltd.',170101);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (7618,'SBI General Insurance Co. Ltd.',103219);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (3035,'Liberty General Insurance Ltd.',239539);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (6327,'Future Generali India Insurance Co. Ltd.',457298);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (9461,'Star Health and Allied Insurrance Co.',260764);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (7459,'HDFC ERGO General Insurance Co. Ltd.�',673912);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (2295,'Bajaj Allianz General Insuarnce Co. Ltd',400558);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (7844,'Cholamandalam MS General Insurance Co. Ltd',180776);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (7825,'Kotak Mahindra General Insurance Company Ltd.',729283);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (7710,'National Insurance Co. Ltd.',274538);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (1839,'Bajaj Allianz General Insurance Co. Ltd.�',482386);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (1409,'Niva Bupa Health Insurance Co. Ltd.',347707);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (7287,'HDFC ERGO General Insurance company Ltd�',213611);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (3827,'United India Insurance Co. Ltd.',625690);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (1999,'Cholamandalam MS General',745083);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (8247,'Care Health Insurance Ltd.',870303);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (8784,'The New India Assurance Co.Ltd.�',240386);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (1583,'Iffco Tokio General Insurance Company Ltd.',608530);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (3489,'ICICI Lombard GI Co. Ltd.',378630);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (6403,'Bajaj Allianz General Insurance Co Ltd�',176147);
+INSERT INTO InsuranceCompany(companyID,companyName,addressID) VALUES (7330,'Future Generali India Insurance Company Limited.���',330866);
