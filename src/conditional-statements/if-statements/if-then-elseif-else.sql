144ee7bebd441c8f53c9657d0c4970cbba16a48d
Akash Das <mfs.akash@gmail.com>
2023-03-29 11:12:37 +0530

    added new files and cheat sheets

--- src/conditional-statements/if-statements/if-then-elseif-else.sql
+++ src/conditional-statements/if-statements/if-then-elseif-else.sql
@@ -1,32 +1,24 @@
-DELIMITER // 
-CREATE PROCEDURE getCustomerLevel(
+-- Active: 1647295028338@@127.0.0.1@3307@classicmodels
+DELIMITER / / CREATE PROCEDURE getCustomerLevel(
     IN pCutomerNumber INT,
     OUT pCustomerLevel VARCHAR(20)
-) 
-BEGIN
-    DECLARE credit DECIMAL(10, 2) DEFAULT 0;
-
-    SELECT creditLimit 
-    INTO credit
-    FROM customers
-    WHERE customerNumber = pCutomerNumber;
-
-    IF credit > 50000 THEN
-        SET pCustomerLevel = 'PLATINUM';
-    ELSEIF credit <= 50000 AND credit > 10000 THEN
-        SET pCustomerLevel = 'GOLD';
-    ELSE
-        SET pCustomerLevel = 'SILVER';
-    END IF;
-END // 
-
-DELIMITER;
-
-CALL getCustomerLevel(114, @level);
+) BEGIN
+DECLARE credit DECIMAL(10, 2) DEFAULT 0;
+SELECT creditLimit INTO credit
+FROM customers
+WHERE customerNumber = pCutomerNumber;
+IF credit > 50000 THEN
+SET pCustomerLevel = 'PLATINUM';
+ELSEIF credit <= 50000
+AND credit > 10000 THEN
+SET pCustomerLevel = 'GOLD';
+ELSE
+SET pCustomerLevel = 'SILVER';
+END IF;
+END / / DELIMITER;
+CALL getCustomerLevel(125, @level);
 SELECT @level;
-
 DROP PROCEDURE getCustomerLevel;
-
 SELECT customerNumber,
     creditLimit
 FROM customers
