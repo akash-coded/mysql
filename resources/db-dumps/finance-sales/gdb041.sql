89cc8dbb27172fee627b8ef2749c506ecc7940c6
Akash Das <mfs.akash@gmail.com>
2024-04-16 22:41:07 +0530

    added db dumps on finance sales data

---
+++ resources/db-dumps/finance-sales/gdb041.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7ddb51e2463a5bb2582417d6d5abb212db145900eadf5021e16bba2d3c6f461
+size 505738947
---
+++ resources/db-dumps/finance-sales/gdb056.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab166be697179be57448ebd4575acc7151e41b96bdc81fc92c89b8ec8bb3aa95
+size 136314411
